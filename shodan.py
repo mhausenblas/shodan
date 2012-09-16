cf54f561f561151ac3d24c7b294c517d99e4be49
Michael Hausenblas <michael.hausenblas@gmail.com>
2012-09-16 12:44:24 +0100

    init HDT handling

--- shodan.py
+++ shodan.py
@@ -8,6 +8,7 @@
 @status: init
 """
 import os, sys, logging, getopt, StringIO, datetime, git
+from subprocess import call
 
 # config
 DEBUG = True
@@ -22,19 +23,22 @@ else:
 
 
 def init_store(storename):
-	"""Creates a directory in user's home containing RDF NTriple & HDT doc, and inits git repo with it"""
+	"""Creates a directory in user's home containing RDF NTriple & HDT doc, and inits git repo with it."""
 	uhome = os.path.expanduser('~') # user home
 	storedir = os.path.join(uhome, storename)
 	storent = os.path.join(storedir, 'datastore.nt')
+	now = datetime.datetime.now()
 	# storehdt = os.path.join(storedir, 'datastore.hdt')
 	
-	if DEBUG: logging.debug("initialising datastore in: %s" %os.path.abspath(storedir))
+	if DEBUG: logging.debug('initialising datastore in: %s' %os.path.abspath(storedir))
 
 	if not os.path.exists(storedir):
 		os.makedirs(storedir)
 		
 	ds = open(storent, 'w')
-	ds.write("");
+	ds.write(''.join(['<file://', os.path.abspath(storedir), '> <http://purl.org/dc/terms/title> "', storename, '" .\n']))
+	ds.write(''.join(['<file://', os.path.abspath(storedir), '> <http://purl.org/dc/terms/creator> <https://github.com/mhausenblas/shodan> .\n']))
+	ds.write(''.join(['<file://', os.path.abspath(storedir), '> <http://purl.org/dc/terms/created> "', now.strftime('%Y-%m-%d'), '" .\n']))
 	ds.close()
 	
 	g = git.Git(os.path.abspath(storedir)) 
@@ -43,25 +47,36 @@ def init_store(storename):
 	commitmsg = 'init store %s' %storename
 	g.commit(m=commitmsg)
 
+# NOTE: can't build HDT due to some funny compiler error, need to check back with Mario
+# for now assume RDF2HDT is done manually (via UI)
+def convert2HDT(ntdoc, htdoc):
+	"""Converts an RDF NTriple document into an HDT document and returns result code from call."""
+	params = '%s %s' %(ntdoc, htdoc)
+	cmd = ''.join(['cd ', HDT_PATH, ' '])
+	cmd += ''.join(['java -server -Xms1024M -Xmx1024M -classpath "', HDT_PATH, '/bin:',  HDT_PATH, '/lib/*" org.rdfhdt.hdt.tools.RDF2HDT ', params])
+	if DEBUG: logging.debug('EXECUTE: %s' %cmd)
+	return call(cmd, shell=True)
 
 def usage():
-	print("Usage: python shodan.py --init | --add | --remove | --match")
-	print("Example: python shodan.py --init example1")
+	print('Usage: python shodan.py --init {storename} | --add | --remove | --match')
+	print('Example: python shodan.py --init ex1')
 
 
 if __name__ == '__main__':
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hi:v", ["help", "init", "verbose"])
+		opts, args = getopt.getopt(sys.argv[1:], 'hi:a:', ['help', 'init', 'add'])
 		for opt, arg in opts:
-			if opt in ("-h", "--help"):
+			if opt in ('-h', '--help'):
 				usage()
 				sys.exit()
-			elif opt in ("-i", "--init"):
+			elif opt in ('-i', '--init'):
 				storename = arg
-				logging.info("Using storename: %s" %storename)
+				logging.info('Creating new store [%s]' %storename)
 				init_store(storename)
-			elif opt in ("-v", "--verbose"): 
-				DEBUG = True
+			elif opt in ('-a', '--add'):
+				params = arg.split(',')
+				logging.info('Adding data to store [%s] %s' %(params[0], params[1]))
+				convert2HDT(params[0], params[1])
 	except getopt.GetoptError, err:
 		print str(err)
 		usage()
